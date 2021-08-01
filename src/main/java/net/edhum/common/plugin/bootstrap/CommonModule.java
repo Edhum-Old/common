6ca68bd36911a33c452d45cfe2a42a40bdf441ec
Juan <choukas@stonepowered.com>
2021-08-01 19:20:31 +0200

    Installed the message module

--- src/main/java/net/edhum/common/plugin/bootstrap/CommonModule.java
+++ src/main/java/net/edhum/common/plugin/bootstrap/CommonModule.java
@@ -4,6 +4,7 @@ import com.google.inject.AbstractModule;
 import net.edhum.common.command.CommandModule;
 import net.edhum.common.configuration.ConfigurationModule;
 import net.edhum.common.i18n.InternalisationModule;
+import net.edhum.common.message.MessageModule;
 import net.edhum.common.persistence.PersistenceModule;
 import net.edhum.common.player.PlayerModule;
 import net.edhum.common.shutdown.ShutdownModule;
@@ -15,6 +16,7 @@ public class CommonModule extends AbstractModule {
         install(new CommandModule());
         install(new ConfigurationModule());
         install(new InternalisationModule());
+        install(new MessageModule());
         install(new PersistenceModule());
         install(new PlayerModule());
         install(new ShutdownModule());
